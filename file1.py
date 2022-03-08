26d1fbd28e5e2d5b40ddbfa05cc5081dcf62b746
shreya1110-dev <shreyasriram00@gmail.com>
2022-03-08 13:43:50 +0530

    files

--- file1.py
+++ file1.py
@@ -5,7 +5,9 @@ import yaml
 from datetime import datetime
 import time
 import pandas as pd
-from collections import MutableMapping
+from collections.abc import MutableMapping
+import multiprocessing
+import threading
 
 def read_yaml():
     with open("Milestone1/Milestone1A.yaml", "r") as stream:
@@ -31,21 +33,89 @@ def read_dict(d: MutableMapping, parent_key: str = '', sep: str ='.'):
             items.append((new_key, v))
     return dict(items)
 
+def sequential(count, workflow_list, time1, f):
+    t = time1
+    for i in range(count, len(workflow_list)):
+        if(workflow_list[i][1] == "Concurrent"):
+            break
+        parser = workflow_list[i][0].split('.')
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="Type" and workflow_list[i][1] == "Task"):
+            timestamp = str(t) + ","
+            timestamp = timestamp + parser[0] + " " + parser[len(parser)-2] + " " + "Entry\n"
+            print("hello")
+            f.write(timestamp)
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="Function"):
+            if(workflow_list[i][1] == "TimeFunction"):
+                timestamp = str(t) + ","
+                timestamp = timestamp + "Executing TimeFunction "
+                arg1 = workflow_list[i+1][1]
+                arg2 = workflow_list[i+2][1]
+                timestamp = timestamp + "(" + arg1 + "," + arg2 + ")\n"
+                f.write(timestamp)
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="ExecutionTime"):
+            print("hi")
+            time.sleep(int(workflow_list[i][1].strip()))
+            t = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            timestamp = t + ","
+            print(parser[len(parser)-2])
+            timestamp = timestamp + parser[0] + " " + parser[len(parser)-3] + " " + "Exit\n"
+            f.write(timestamp)
+
+def concurrent(count, workflow_list, time1, f):
+    t = time1
+    for i in range(count, len(workflow_list)):
+        if(workflow_list[i][1] == "Sequential"):
+            break
+        parser = workflow_list[i][0].split('.')
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="Type" and workflow_list[i][1] == "Task"):
+            timestamp = str(t) + ","
+            timestamp = timestamp + parser[0] + " " + parser[len(parser)-2] + " " + "Entry\n"
+            f.write(timestamp)
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="Function"):
+            if(workflow_list[i][1] == "TimeFunction"):
+                timestamp = str(t) + ","
+                timestamp = timestamp + "Executing TimeFunction "
+                arg1 = workflow_list[i+1][1]
+                arg2 = workflow_list[i+2][1]
+                timestamp = timestamp + "(" + arg1 + "," + arg2 + ")\n"
+                f.write(timestamp)
+        if(parser[1] == "Activities" and parser[len(parser)-1]=="ExecutionTime"):
+            t = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            timestamp = t + ","
+            timestamp = timestamp + parser[0] + " " + parser[len(parser)-4] + " " + "Exit\n"
+            f.write(timestamp)
+
 def print_log():
     data = read_yaml()
     workflow = read_dict(data)
     timestamp = ''
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     timestamp += timestamp + datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     timestamp += ','
     key = data.keys()
     for k in key:
         timestamp += k
-        timestamp += " Entry"
+        timestamp += " Entry\n"
     f = open("file1.txt", "a")
     f.write(timestamp)
-    for k,v in workflow.items():
-        parser = k.split('.')
-        if(parser[len(parser)-1] == "Execution"):
-            if(workflow[k] == "")
-
+    count = 0
+    workflow_list = list(workflow.items())
+    for i in range(0, len(workflow_list)):
+        print(workflow_list[i])
+    for i in range(0, len(workflow_list)):
+        parser = workflow_list[i][0].split('.')
+        if(workflow_list[i][1] == "Sequential" and parser[len(parser)-1]=="Execution"):
+            sequential(count, workflow_list, time, f)
+        else:
+            concurrent(count, workflow_list, time, f)
+        count = count+1
+    timestamp = ''  
+    timestamp += timestamp + datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    timestamp += ','
+    key = data.keys()
+    for k in key:
+        timestamp += k
+        timestamp += " Entry\n"
+    f.write(timestamp)      
+    f.close()
 print_log()
